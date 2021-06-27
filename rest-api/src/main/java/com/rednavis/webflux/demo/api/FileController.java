fdd337857eba6732c2b64dc14be8193e3f9cc997
@andreylevonchik <andrei.liavonchyk@rednavis.com>
2021-06-27 21:02:57 +0300

    Refactoring FileController

--- rest-api/src/main/java/com/rednavis/webflux/demo/api/FileController.java
+++ rest-api/src/main/java/com/rednavis/webflux/demo/api/FileController.java
@@ -11,30 +11,42 @@ import reactor.core.scheduler.Schedulers;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 @RequestMapping("/upload")
 public class FileController {
-    @PostMapping(value = "/")
-    public Flux<String> upload(@RequestPart("file") Flux<FilePart> file) throws IOException {
+    /**
+     * Metho save file to temporary file and returns the size in bytes
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    //   @PostMapping(value = "/")
+    @RequestMapping(
+            method = POST,
+            path = "/",
+            consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}
+    )
+    public Flux<String> upload(@RequestPart("file") Flux<FilePart> file)  {
         Flux<String> temp = file.concatMap(filePart -> createTempFile(filePart.filename())
                 .flatMap(tempFile -> filePart.transferTo(tempFile)
                         .then(Mono.just("File length: " + tempFile.toFile().length()))));
         return temp;
-
     }
 
-    @GetMapping
-    @ResponseBody
-    public String status() {
-        return "FileController" ;
-    }
+    private Mono<Path> createTempFile(String fileName){
 
-    private Mono<Path> createTempFile(String suffix) {
-        return Mono.defer(() -> {
-            return Mono.just(new File("./rest-api/src/main/resources/" + suffix ).toPath());
-        })
-                .subscribeOn(Schedulers.boundedElastic());
+        try {
+            final File tempFile = File.createTempFile(fileName, "");
+            return Mono.defer(() -> {
+                return Mono.just(tempFile.toPath());
+            })
+                    .subscribeOn(Schedulers.boundedElastic());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
